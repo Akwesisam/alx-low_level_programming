feaea397dde5c37be93d6d0553007741fcd370d5
Akwesisam <robagy12@gmail.com>
2023-11-03 19:38:54 -0700

    12

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,15 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s", "Fizz Buzz");
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 			printf("%s", "Fizz");
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 			printf("%s", "Buzz");
 		else
 			printf("%d", i);
-		if (i != 100)
-			printf(" ");
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
